227664af148c350dcad163e020fd441538a9ad5d
Ryan Maxwell <ryanm@xwell.co.nz>
2015-03-21 13:05:18 +1300

    fix indentation

--- UIAlertController+Blocks.h
+++ UIAlertController+Blocks.h
@@ -34,31 +34,31 @@ typedef void (^UIAlertControllerCompletionBlock) (UIAlertController * __nonnull 
 @interface UIAlertController (Blocks)
 
 + (nonnull instancetype)showInViewController:(nonnull UIViewController *)viewController
-                           withTitle:(nullable NSString *)title
-                             message:(nullable NSString *)message
-                      preferredStyle:(UIAlertControllerStyle)preferredStyle
-                   cancelButtonTitle:(nullable NSString *)cancelButtonTitle
-              destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
-                   otherButtonTitles:(nullable NSArray *)otherButtonTitles
-  popoverPresentationControllerBlock:(nullable UIAlertControllerPopoverPresentationControllerBlock)popoverPresentationControllerBlock
-                            tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
+                                   withTitle:(nullable NSString *)title
+                                     message:(nullable NSString *)message
+                              preferredStyle:(UIAlertControllerStyle)preferredStyle
+                           cancelButtonTitle:(nullable NSString *)cancelButtonTitle
+                      destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
+                           otherButtonTitles:(nullable NSArray *)otherButtonTitles
+          popoverPresentationControllerBlock:(nullable UIAlertControllerPopoverPresentationControllerBlock)popoverPresentationControllerBlock
+                                    tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
 
 + (nonnull instancetype)showAlertInViewController:(nonnull UIViewController *)viewController
-                                withTitle:(nullable NSString *)title
-                                  message:(nullable NSString *)message
-                        cancelButtonTitle:(nullable NSString *)cancelButtonTitle
-                   destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
-                        otherButtonTitles:(nullable NSArray *)otherButtonTitles
-                                 tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
+                                        withTitle:(nullable NSString *)title
+                                          message:(nullable NSString *)message
+                                cancelButtonTitle:(nullable NSString *)cancelButtonTitle
+                           destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
+                                otherButtonTitles:(nullable NSArray *)otherButtonTitles
+                                         tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
 
 + (nonnull instancetype)showActionSheetInViewController:(nonnull UIViewController *)viewController
-                                      withTitle:(nullable NSString *)title
-                                        message:(nullable NSString *)message
-                              cancelButtonTitle:(nullable NSString *)cancelButtonTitle
-                         destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
-                              otherButtonTitles:(nullable NSArray *)otherButtonTitles
-             popoverPresentationControllerBlock:(nullable UIAlertControllerPopoverPresentationControllerBlock)popoverPresentationControllerBlock
-                                       tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
+                                              withTitle:(nullable NSString *)title
+                                                message:(nullable NSString *)message
+                                      cancelButtonTitle:(nullable NSString *)cancelButtonTitle
+                                 destructiveButtonTitle:(nullable NSString *)destructiveButtonTitle
+                                      otherButtonTitles:(nullable NSArray *)otherButtonTitles
+                     popoverPresentationControllerBlock:(nullable UIAlertControllerPopoverPresentationControllerBlock)popoverPresentationControllerBlock
+                                               tapBlock:(nullable UIAlertControllerCompletionBlock)tapBlock;
 
 @property (readonly, nonatomic) BOOL visible;
 @property (readonly, nonatomic) NSInteger cancelButtonIndex;

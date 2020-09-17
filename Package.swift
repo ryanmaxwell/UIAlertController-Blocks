1c3b41a31f6a02f117f0748e664240ca7622e8e7
Sourabh Gupta <sgupta@tigerconnect.com>
2020-09-17 23:41:43 +1200

    Fix source path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIAlertControllerBlocks",
-            path: "lottie-swift/src",
             dependencies: [])
     ]
 )

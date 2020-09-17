abd4c28f17dcce12f5216ebf36f109d61863ea71
Sourabh Gupta <sgupta@tigerconnect.com>
2020-09-18 07:06:22 +1200

    Update target name

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIAlertControllerBlocks",
+    name: "UIAlertController_Blocks",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "UIAlertControllerBlocks",
-            targets: ["UIAlertControllerBlocks"]),
+            name: "UIAlertController_Blocks",
+            targets: ["UIAlertController_Blocks"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "UIAlertControllerBlocks",
+            name: "UIAlertController_Blocks",
             dependencies: [])
     ]
 )

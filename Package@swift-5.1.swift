6f9bbce4ff02a78ad2e598bcd6725a946b4aac7f
Sourabh Gupta <sgupta@tigerconnect.com>
2020-09-18 06:44:09 +1200

    Add header path

--- Package@swift-5.1.swift
+++ Package@swift-5.1.swift
@@ -21,6 +21,7 @@ let package = Package(
         .target(
             name: "UIAlertControllerBlocks",
             path: ".",
+            publicHeadersPath: ".",
             dependencies: [])
     ]
 )

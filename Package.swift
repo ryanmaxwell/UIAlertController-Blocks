89abd277a578a46bd5abdcea4361b420d47a3bdc
Sourabh Gupta <sgupta@tigerconnect.com>
2022-09-13 15:14:54 +1200

    Update swift tools version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "UIAlertController_Blocks",
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "UIAlertController_Blocks",
